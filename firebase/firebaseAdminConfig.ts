27b858d95f2293ea43c23f3885f57e93687561b1
Patao <imaimizuki150205@yahoo.co.jp>
2021-05-20 17:46:49 +0900

    delete unnesesally files

--- firebase/firebaseAdminConfig.ts
+++
@@ -1,9 +0,0 @@
-import * as admin from "firebase-admin";
-
-const app = admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: "https://studious.firebaseio.com",
-});
-
-const adminAuth = app.auth();
-// adminAuth.
